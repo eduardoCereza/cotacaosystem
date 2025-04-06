66188dfaf582f6a3963dab5f28d53092f361954e
Eduardo <eduardo.cereza@outlook.com>
2025-04-06 14:11:42 -0300

    importando os valores do JSON
    fui no site e peguei em qual lugar se encontra o valor da cotacao. Descobri que é no BID, ent coloquei nos respectivos conversores

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ url = 'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL'
 app = requests.get(url)
 
 app = app.json()
+cotacao_dolar = input(app["USDBRL"]['bid'])
+cotacao_euro = input(app["EURBRL"]['bid'])
+cotacao_bitcoin = input(app["BTCBRL"]['bid'])
