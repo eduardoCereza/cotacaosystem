c70783620f9e3335ad0018998590db0fba2fe58a
Eduardo <eduardo.cereza@outlook.com>
2025-04-06 14:15:13 -0300

    comentando partes

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 #importando bibliotecas
 import requests
 
+#define a URL como a API
 url = 'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL'
+#pega(get) a API para podermos manipula-la e achar os valores
 app = requests.get(url)
 
+#importa o JSON
 app = app.json()
+#atribui a cada cotacao o seu valor, podendo ser achada no arquivo JSON
 cotacao_dolar = input(app["USDBRL"]['bid'])
 cotacao_euro = input(app["EURBRL"]['bid'])
 cotacao_bitcoin = input(app["BTCBRL"]['bid'])
 
+#sistema de opcao
 print("Digite o número que corresponde ao conversor: ")
 print('1')
 print('2')
 print('3')
+
+#logica em loop condicional
